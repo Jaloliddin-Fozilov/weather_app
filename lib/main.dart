be24280313944902131866ef5491ffaf9722f682
Jaloliddin-Fozilov <jaloliddinish@gmail.com>
2022-07-14 22:26:15 +0500

    UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:http/http.dart';
-import 'package:weather_app/logic/repositories/weather_repository.dart';
-import 'package:weather_app/logic/services/https/weather_api_services.dart';
 
+import 'logic/cubits/cubit/weather_cubit.dart';
+import 'logic/repositories/weather_repository.dart';
+import 'logic/services/https/weather_api_services.dart';
 import 'presentation/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _getWeatherData();
-  }
-
-  void _getWeatherData() async {
-    final weatherRep = WeatherRepository(
-      weatherApiServices: WeatherApiServices(
-        client: Client(),
-      ),
-    );
-    final weather = await weatherRep.getWeather('chirchiq');
-    print(weather.temperature);
-    print(weather.main);
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return RepositoryProvider(
+      create: (context) => WeatherRepository(
+        weatherApiServices: WeatherApiServices(
+          client: Client(),
+        ),
       ),
-      home: const HomeScreen(),
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (ctx) => WeatherCubit(
+              weatherRepository: ctx.read<WeatherRepository>(),
+            ),
+          ),
+        ],
+        child: MaterialApp(
+          title: 'Weather app',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: const HomeScreen(),
+        ),
+      ),
     );
   }
 }
--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,10 +1,149 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/logic/cubits/cubit/weather_cubit.dart';
+import '../../helpers/extensions/string_extensions.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<WeatherCubit>().getWeather('moscow');
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: BlocConsumer<WeatherCubit, WeatherState>(
+        listener: (ctx, state) {
+          if (state is WeatherError) {
+            showDialog(
+              context: context,
+              builder: (ctx) => AlertDialog(
+                title: const Text('Error'),
+                content: Text(state.message),
+                actions: [
+                  TextButton(
+                    onPressed: () => Navigator.of(context).pop(),
+                    child: const Text('OK'),
+                  )
+                ],
+              ),
+            );
+          }
+        },
+        builder: (ctx, state) {
+          if (state is WeatherInitial) {
+            return const Center(
+              child: Text('Select a city'),
+            );
+          }
+          if (state is WeatherLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (state is WeatherLoaded) {
+            final weather = state.weather;
+            String mainWeather = weather.main.toLowerCase();
+            String imagePath = '';
+            if (mainWeather.contains('sun')) {
+              imagePath = 'assets/sunny.jpg';
+            } else if (mainWeather.contains('cloud')) {
+              imagePath = 'assets/cloudy.jpg';
+            } else if (mainWeather.contains('rain')) {
+              imagePath = 'assets/rainy.jpg';
+            } else {
+              imagePath = 'assets/night.jpg';
+            }
+            return Stack(
+              children: [
+                Image.asset(
+                  imagePath,
+                  fit: BoxFit.cover,
+                  height: double.infinity,
+                ),
+                Container(
+                  color: Colors.black.withOpacity(0.4),
+                ),
+                SafeArea(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              weather.city.capitalizeString(),
+                              style: const TextStyle(
+                                fontSize: 40,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              weather.description,
+                              style: const TextStyle(
+                                fontSize: 16,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '${weather.temperature.toStringAsFixed(0)}℃',
+                              style: const TextStyle(
+                                fontSize: 70,
+                                color: Colors.white,
+                              ),
+                            ),
+                            Row(
+                              children: [
+                                Container(
+                                  width: 40,
+                                  height: 40,
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey,
+                                    borderRadius: BorderRadius.circular(50),
+                                  ),
+                                  child: Image.network(
+                                    'http://openweathermap.org/img/wn/${weather.icon}@2x.png',
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                                Text(
+                                  weather.main,
+                                  style: const TextStyle(
+                                    fontSize: 30,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            );
+          }
+          return Container();
+        },
+      ),
+    );
   }
 }
